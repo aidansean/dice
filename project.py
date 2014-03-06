c92771cb5cb441a311344e72723ebeee8628ceea
aidansean <aidan.randleconde@gmail.com>
2014-03-06 22:49:09 +0100

    Renamed project.py

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 from project_module import project_object, image_object, link_object, challenge_object
 
-p = project_object('dice')
+p = project_object('dice', 'Dice simulator')
 p.domain = 'http://www.aidansean.com/'
 p.path = 'dice'
 p.preview_image_ = image_object('http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg', 408, 287)
@@ -17,5 +17,3 @@ p.overview = '''There are three modes:
 The fair dice setting is straightforward, the fudge dice have \\([-1,-1,0,0,+1,+1]\\), and the xkcd dice always return \(4\), which is an xkcd in-joke.  The colors can be chosen to match the fudge dice colours we used in the RPG, and since one of our players had OCD and we'd sometimes exchange dice colors to wind him up, there was an option to choose colors randomly.'''
 
 p.challenges.append(challenge_object('Picking random rgb values for the colors means that black is not always the best choice of text color.', 'Add a few lines to see how dark a color is, and change the text to white where suitable.', 'To be done'))
-
-print p.wordpress_text()
